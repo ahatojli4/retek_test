a9361eb77317837a828d0a888d947ad391c41513
Denis Borisevichus <borideni@gmail.com>
2017-07-12 11:01:39 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Problem:
+
+> Create a CLI-based application that would:
+> - take a folder path as argument;
+> - count words in all text files in the given folder in threaded way 
+> - one thread per file;
+> - print total counter to output.
+
+PHP70 compiling with ZTS mode. Used [pthread](http://php.net/manual/en/book.pthreads.php) lib.
+
+**Usage examples:**
+
+<pre><code>php wordsCounter.php
+Need a path for file or dir.</code></pre>
+
+<pre><code>php wordsCounter.php test
+Numbers of words: 6</code></pre>
+
+<pre><code>php wordsCounter.php test asdf
+Only first argument is used.
+Numbers of words: 6</code></pre>
+
+for mac: `brew install homebrew/php/php70-pthreads`
---
+++ wordsCounter.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php
+<?php
+
+class ThreadWordCounter extends Thread
+{
+
+	/**
+	 * @var int
+	 */
+	private $numberOfWords = 0;
+	/**
+	 * @var string
+	 */
+	private $filePath;
+
+	/**
+	 * ThreadWordCounter constructor.
+	 * @param string $filePath
+	 */
+	public function __construct(string $filePath)
+	{
+		$this->filePath = $filePath;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getNumberOfWords(): int
+	{
+		return $this->numberOfWords;
+	}
+
+	public function run()
+	{
+		if (file_exists($this->filePath)) {
+			$this->numberOfWords = $this->countWordsViaPHP($this->filePath);
+//			$this->numberOfWords = $this->countWordsViaWC($this->filePath);
+		}
+	}
+
+	/**
+	 * @param string $filePath
+	 * @return int
+	 */
+	private function countWordsViaPHP(string $filePath): int
+	{
+		return (int)str_word_count(file_get_contents($filePath));
+	}
+
+	/**
+	 * Bad way. It used a hint with string conversion to numbers, if string start with number.
+	 * @param string $filePath
+	 * @return int
+	 */
+	private function countWordsViaWC(string $filePath): int
+	{
+		return (int)exec('wc -w ' . $filePath);
+	}
+}
+
+if ($argc === 1) {
+	echo "Need a path for file or dir.\n";
+	die;
+} elseif ($argc > 2) {
+	echo "Only first argument is used.\n";
+}
+
+
+list(, $filePath) = $argv;
+if (!file_exists($filePath)) {
+	echo "Wrong path!\n";
+}
+
+$recursiveII = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($filePath), RecursiveIteratorIterator::SELF_FIRST);
+/** @var ThreadWordCounter[] $wordCounterObjects */
+$wordCounterObjects = [];
+/** @var \SplFileInfo $element */
+foreach ($recursiveII as $element) {
+	if ($element->isDir()) {
+		continue;
+	}
+	$object = new ThreadWordCounter($element->getPathname());
+	$object->start();
+	$wordCounterObjects[] = $object;
+}
+
+
+foreach ($wordCounterObjects as $object) {
+	$object->join();
+}
+
+$numberOfWords = 0;
+foreach ($wordCounterObjects as $object) {
+	$numberOfWords += $object->getNumberOfWords();
+}
+
+echo "Numbers of words: " . $numberOfWords . "\n";
